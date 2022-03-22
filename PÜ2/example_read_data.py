c4b8f4f8810afc0f7a2222cad1a81edc26880ba7
KlangM <melvinauage@gmail.com>
2022-03-22 09:29:33 +0100

    Übung 2

--- PÜ2/example_read_data.py
+++ PÜ2/example_read_data.py
@@ -8,6 +8,8 @@ for i in range(3):
      x = ('input_data/power_data_'+m +'.txt')
      power_data_watts = open(x).read().split("\n")
      y = np.array(power_data_watts)
-     plt.title("Line graph"+str(i +1))
+     plt.title("Line graph" +str(i+1))
      plt.plot(y, color="green")
      plt.show()
+
+# %%
--- Übung 2.py
+++ Übung 2.py
@@ -1,12 +1,14 @@
-# %% Import der nötigen Pakete
+#%% #importieren von nötigen Bibliotheken
 import numpy as np
 import matplotlib.pyplot as plt
 
-# %%
-for i in range(3):
-    x = ('PÜ2\input_data\power_data_'+str(i +1) +'.txt')
-    print(x)
-# %%
-
-
+#%% #schleife zum importieren der Daten
+for i in range(3):                         
+     m = str(i + 1)
+     x = ('PÜ2\input_data\power_data_'+str(i +1) +'.txt')
+     power_data_watts = open(x).read().split("\n")
+     y = np.array(power_data_watts)
+     plt.title("Line graph" +str(i+1))
+     plt.plot(y, color="green")
+     plt.show()
 # %%
