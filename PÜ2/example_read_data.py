bd096f7c3ad7cc39a3083841205725abb06d3885
KrefftMoritz <101808353+KrefftMoritz@users.noreply.github.com>
2022-03-22 09:20:14 +0100

    Update example_read_data.py

--- PÜ2/example_read_data.py
+++ PÜ2/example_read_data.py
@@ -1,29 +1,13 @@
-# %% Import der nötigen Pakete
+#%%
 import numpy as np
 import matplotlib.pyplot as plt
 
-# %% Öffnen der Datei und konvertieren zu numpy-Array
-file_name =  'input_data/power_data_1.txt'
-power_data_watts = open(file_name).read().split("\n")
-x = np.array(power_data_watts)
-
-# %% Erstellen des Plots
-plt.title("Line graph")
-plt.plot(x, color="red")
-plt.show()
-
-
-# %%
-for i in range(3):                                #die schleife spuckt uns die 3 arrays raus                          
+#%%
+for i in range(3):                         
      m = str(i + 1)
      x = ('input_data/power_data_'+m +'.txt')
      power_data_watts = open(x).read().split("\n")
      y = np.array(power_data_watts)
-     print(y)
-     print(x)
-# %%
-for i in range(3):                                #die graphen sind nur von einem array des müssen wir noch ändern
-    plt.title("Line graph"+str(i +1))
-    plt.plot(y, color="green")
-    plt.show()
-# %%
+     plt.title("Line graph"+str(i +1))
+     plt.plot(y, color="green")
+     plt.show()
