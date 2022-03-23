41bc2bdbe8da03240043f45d80519599fa60a237
KrefftMoritz <m.krefft@mci4me.at>
2022-03-23 11:37:51 +0100

    Comments

--- PÜ2/main_1_comments_only.py
+++ PÜ2/main_1_comments_only.py
@@ -1,15 +1,15 @@
-# UC 2.0. - Auswertung der Leistungsdaten
-    # UC 2.1. - Einlesen der Daten
+### UC 2.0. - Auswertung der Leistungsdaten
+    ## UC 2.1. - Einlesen der Daten
         # Die Daten vom EKG werden eingelesen
-    # UC 2.2. - Vorverarbeiten der Daten
+    ## UC 2.2. - Vorverarbeiten der Daten
         # Die eingelesenen Daten werden weiter verarbeitet
-    # UC 2.3. - Analysieren der Daten auf Abbruchkriterium
+    ## UC 2.3. - Analysieren der Daten auf Abbruchkriterium
         # Es wird geprüft ob das EKG abgebrochen wurde
-    # UC 2.4. - Erstellen einer Zusammenfassung
+    ## UC 2.4. - Erstellen einer Zusammenfassung
         # Die Daten werden Zusammengefasst
-    # UC 2.5. - Visualisierung der Daten
+    ## UC 2.5. - Visualisierung der Daten
         # Die zusammengefassten Daten werden geplottet
-    # UC 2.6. - Manuelle Eingabe eines Abbruch-Kriteriums
+    ## UC 2.6. - Manuelle Eingabe eines Abbruch-Kriteriums
         #
-    # UC 2.7. - Speicherung der Daten
+    ## UC 2.7. - Speicherung der Daten
         # Daten werden gespeichert
